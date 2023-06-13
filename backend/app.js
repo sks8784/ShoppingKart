4da33f6f5b597d07e930ddcb9c7b4e9fddd8c83f
Shubham Kumar Sinha <shubhamsinhaasus@gmail.com>
2023-06-13 21:35:26 +0530

    changes made

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,6 @@ const app=express();
 const cookieParser=require("cookie-parser");
 const bodyParser=require("body-parser");
 const fileUpload=require("express-fileupload");
-const cors=require("cors");
 
 const errorMiddleware=require("./middleware/error");
 
@@ -13,7 +12,6 @@ if(process.env.NODE_ENV !== "PRODUCTION"){
 }
 
 app.use(express.json({limit: '50mb'}));
-app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(fileUpload());
